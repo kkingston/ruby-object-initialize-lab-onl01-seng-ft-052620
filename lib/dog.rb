97257308166e2ea563bf23cf0b2c6aebe625cb0b
Kayla Kingston <kkingston09@gmail.com>
2020-06-08 00:15:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,12 +3,12 @@ class Dog
     @breed = breed 
     @name = name
   end
-  def name=(name)
-    @name = name
-  end
-  def name
-    @name
-  end
+  # def name=(name)
+  #   @name = name
+  # end
+  # def name
+  #   @name
+  # end
 end 
 
 
